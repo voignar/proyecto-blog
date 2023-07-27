856f18b2d1bcaa1a3eb21dfbca1ae91460b2beed
Israel Cabo <voignar@gmail.com>
2023-07-27 20:42:31 +0200

    estilizando menu superior

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link"
 export function Header() {
     return(
-        <nav>
-            <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/posts">Posts</Link></li>
+        <nav className="flex content-center justify-between flex-wrap bg-stone-800 p-6">
+            <ul className="w-full flex justify-center content-center">
+                <li className="text-stone-400 hover:text-white mr-4"><Link href="/">Home</Link></li>
+                <li className="text-stone-400 hover:text-white mr-4"><Link href="/about">About</Link></li>
+                <li className="text-stone-400 hover:text-white mr-4"><Link href="/posts">Posts</Link></li>
             </ul>
         </nav>
     )
