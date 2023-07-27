38bbde3b0dcec1582a867cdc7ef1d4bd745b4938
Israel Cabo <voignar@gmail.com>
2023-07-27 22:05:39 +0200

    agregado botón volver en el post individual

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+import { useRouter } from "next/navigation"
+
 type Props = {
     params: { id: number }
   }
@@ -10,9 +13,17 @@ export function generateMetadata({ params }: Props)
 }
 
 export default function PagePostID({ params }: Props) {
+    const router = useRouter()
     return (
         <div>
-          <h1>Post ID: {params.id}</h1>
+          <h1 className="text-xl mb-2">Post ID: {params.id}</h1>
+          <button className="rounded text-sm bg-indigo-600 text-white p-1.5 m-1.5" 
+                    onClick={() => {
+                        router.push('/posts')
+                    }}>
+            Volver
+          </button>
         </div>
+
     )
 }
