b052fe491e2f9af180f74b592bddb22d5b0b3bb3
Israel Cabo <voignar@gmail.com>
2023-07-27 22:19:43 +0200

    mapeo de las categorias en posts

--- src/app/posts/category/[[...slug]]/page.tsx
+++ src/app/posts/category/[[...slug]]/page.tsx
@@ -1,5 +1,13 @@
-export default function CategoryPost(){
+export default function CategoryPost({ params }: { params: { slug: string[] } }){
     return(
-        <div>Post Cateogry</div>
+        <div>
+            Post Cateogry:
+           {
+           params.slug?.map((slug:string, index:number)=>(
+                
+                    <h2 key={`cat-${index}`}>{slug}</h2>
+                )) 
+            }
+        </div>
     )
 }
